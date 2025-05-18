f954b50af43178bd61dd562824f74677670697e5
ammarinboiman <s6652C10003@sau.ac.th>
2025-05-18 17:19:25 +0700

    complete

--- lib/views/result_screen_ui.dart
+++ lib/views/result_screen_ui.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class ResultScreenUi extends StatefulWidget {
-  const ResultScreenUi({super.key});
+// สร้างตัวเเปรรับค่าที่จะส่งมา
+  double? carPrice;
+  double? payPerMonth;
+  int? month;
+
+// เอาตัวเเปรที่สร้างมารอรับค่าที่ส่งมา
+
+  ResultScreenUi({
+    super.key,
+    this.carPrice,
+    this.payPerMonth,
+    this.month,
+  });
 
   @override
   State<ResultScreenUi> createState() => _ResultScreenUiState();
@@ -10,6 +23,84 @@ class ResultScreenUi extends StatefulWidget {
 class _ResultScreenUiState extends State<ResultScreenUi> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    // กำหนดรูปเเบบของตัวเลข ณ ที่นี้ คอมมาคั่นหลักพัน / ล้าน เเละ ทศนิยม 2 ตำเเหน่ง
+    NumberFormat fm = NumberFormat("#,###,##0.00");
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text(
+          'CI Calculator',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'ผ่อนทั้งหมด ',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '${widget.month}',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+                Text(
+                  ' เดือน',
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              'ค่างวดรถต่อเดือน',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '${fm.format(widget.payPerMonth)}',
+              style: TextStyle(
+                fontSize: 50.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            ),
+            Text(
+              'บาท',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/views/splash_screen_ui.dart
+++ lib/views/splash_screen_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_car_installment_calculator/views/input_screen_ui.dart';
 
 class SplashScreenUi extends StatefulWidget {
   const SplashScreenUi({super.key});
@@ -8,6 +9,18 @@ class SplashScreenUi extends StatefulWidget {
 }
 
 class _SplashScreenUiState extends State<SplashScreenUi> {
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const InputScreenUI(),
+        ),
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
